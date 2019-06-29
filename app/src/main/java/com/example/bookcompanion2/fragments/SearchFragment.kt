51697761bf912b118c1e9bdcd25c75316ee9f0ce
Denny Casabianca <casabiancadenny@gmail.com>
2019-06-29 15:29:36 +0200

    Search Buttong Logic implementation

--- app/src/main/java/com/example/bookcompanion2/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/bookcompanion2/fragments/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -22,6 +23,17 @@ class SearchFragment : Fragment() {
         val binding: SearchFragmentBinding = DataBindingUtil.inflate(
             inflater, R.layout.search_fragment, container, false
         )
+
+        val editView = binding.bookNameEditView
+
+        binding.searchButton.setOnClickListener { v ->
+            if(!editView.text.isNullOrBlank()){
+                Toast.makeText(context,editView.text.toString(),Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(context,"Please insert a title first",Toast.LENGTH_SHORT).show()
+            }
+        }
 
         return binding.root
     }
